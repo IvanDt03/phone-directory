dff8236761f3cd5cfbeb9b89e54497fb93e533be
Ivan Dmitirev <vanya2003569@yandex.ru>
2024-04-22 23:22:12 +0300

    Добавить поля, определение методов в класс HashTable

--- WinPhoneDirectory/HashTable.cs
+++ WinPhoneDirectory/HashTable.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace WinPhoneDirectory
 {
+    // Разрешение коллизий будет осуществляться при помощи метода открытой адресации
     internal class HashTable
     {
+        private int _sizeTable; // размер таблицы
+        private readonly static int _step = 37; // шаг для разрешения коллизий
+        private int _curSize; // количество элментов в таблице
+        private HashItem[] _hashTable; // хеш-таблица
+
+        public HashTable(int sizeTable) { }
+        public void HashInit() { }
+        private int HashFunction(string s) { }
+        public int AddItem(string FIO, string phoneNumer) { }
+        private void ClearVisit() { } // метод отчистки полей посещения
+        public bool RemoveItem(string phoneNumber, out int index) { }
+        public int FindItem(string phoneNumber, out string FIO, out int count) { }
     }
 }
