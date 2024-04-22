0c16ab842c3553a3d914d886b84acc3a0c18b88f
Ivan Dmitirev <vanya2003569@yandex.ru>
2024-04-22 23:03:50 +0300

    Определить классы Person, HashItem, HashTable, Form1

---
+++ WinPhoneDirectory/HashItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WinPhoneDirectory
+{
+    internal struct HashItem
+    {
+        public Person _person;
+        public bool _empty; // признак занятости
+        public bool _visit; // признак посещения
+    }
+}
---
+++ WinPhoneDirectory/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace WinPhoneDirectory
+{
+    internal class Person
+    {
+        public Person(string phoneNUmber, string FIO)
+        {
+            _phoneNumber = phoneNUmber;
+            this.FIO = FIO;
+        }
+        public Person() : this(String.Empty, String.Empty) { }
+
+        private string _phoneNumber;
+        public string FIO { get; set; }
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                if (value.Length != 11) throw new ArgumentException("Invalid phone number");
+                _phoneNumber = value;
+            }
+        }
+    }
+}
