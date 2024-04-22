4cb07c6eaa643561aee284a57f1170698f99c079
Ivan Dmitirev <vanya2003569@yandex.ru>
2024-04-22 23:47:54 +0300

    Добавить реализацию свойства SizeTable, мтода HashInit, конструктора HashTable(int sizeTable)

--- WinPhoneDirectory/HashTable.cs
+++ WinPhoneDirectory/HashTable.cs
@@ -10,12 +10,36 @@ namespace WinPhoneDirectory
     internal class HashTable
     {
         private int _sizeTable; // размер таблицы
+        public int SizeTable
+        {
+            get => _sizeTable;
+            set
+            {
+                if (value < 0 || value < _curSize) 
+                    throw new ArgumentException("The length of the hash table cannot be less than zero");
+                _sizeTable = value;
+            }
+        }
         private readonly static int _step = 37; // шаг для разрешения коллизий
         private int _curSize; // количество элментов в таблице
         private HashItem[] _hashTable; // хеш-таблица
 
-        public HashTable(int sizeTable) { }
-        public void HashInit() { }
+        public HashTable(int sizeTable) 
+        {
+            SizeTable = sizeTable;
+            _hashTable = new HashItem[SizeTable];
+            HashInit();
+        }
+        public void HashInit() 
+        {
+            _curSize = 0;
+
+            for (int i = 0; i < SizeTable; ++i)
+            {
+                this._hashTable[i]._empty = true;
+                this._hashTable[i]._visit = false;
+            }
+        }
         private int HashFunction(string s) 
         {
             int result = 0;
